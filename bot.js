7812b4981baf62008d6f65978768ebfbc93340f4
mwear3 <35821579+mwear3@users.noreply.github.com>
2018-11-17 20:42:24 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
-yyy
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '$index') {
+    	message.reply('this is a test');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
